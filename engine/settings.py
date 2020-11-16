d543e54e2ccb83ca6779b9a6dc4ae3b5db6b9724
Levi Boy <holop.from.talos@gmail.com>
2020-11-16 17:37:00 +0500

    test heroku image seving

--- engine/settings.py
+++ engine/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'engine.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=None, ssl_require=True)
 }
 
 # Password validation
